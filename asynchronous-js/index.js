9a2e4021514aff5dcabc3d604d67af5422f77a53
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-08-03 23:37:48 +0300

    simple example of callback hell

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,47 +1,12 @@
-/*
-- Asynchronous is a process that allows an application to run a second set
-    of instructions while focusing on it's primary or basic process. 
-    - it has several benefits including improved application performance,
-    wide application to different coding languages, and better user experience.
-    - A few use cases for asynchronous include reduced inefficiencies from an 
-    application and efficient data collection.
-
-
- */
-
-function task1 (callback) {
-    setTimeout( () => {
-        console.log('1. Create index.html file')
-        callback();
-    }, 1000);
+const showMessage = (callback) =>
+    console.log(callback);
+const firstMessage = (callback) => {
+    setTimeout(() => {
+    console.log('Hello');
+    callback();
+    }, 1000)
 };
-function task2 (callback) {
-    setTimeout( () => {
-        console.log('2. Create styles.css file')
-        callback();
-    }, 2000);
+const secondMessage = () => {
+    showMessage('World!')
 };
-function task3 (callback) {
-    setTimeout( () => {
-        console.log('3. Create script.js file')
-        callback();
-    }, 500);
-};
-function task4 (callback) {
-    setTimeout( () => {
-        console.log('4. Link all three files')
-        callback();
-    }, 4000);    
-};
-//callback hell
-task1(() =>{
-    task2(() => {
-        task3(() => {
-            task4(()=>{
-
-            })
-        })
-    })
-})
-
-
+firstMessage(secondMessage);
