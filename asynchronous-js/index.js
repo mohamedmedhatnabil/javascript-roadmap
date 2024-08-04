5cdbb7983c222ab9d1e02739e6d4ffef59307b42
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-08-04 23:01:16 +0300

    getAllPRoducts Function
    function used to get all the products

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,6 +1,10 @@
-fetch('https://dummyjson.com/products/1', {
-    method: 'DELETE',
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log(error)) 
+const getAllProducts = async () => {
+    try{
+    const response = await fetch('https://dummyjson.com/products/');
+    const json = await response.json();
+    console.log(json)
+    } catch(error) {
+        console.log(error);
+    }
+}
+getAllProducts();
