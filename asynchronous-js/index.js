7d486552f7a6c97f9bf74fdfa8323178c896cc8d
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-08-04 19:16:42 +0300

    promise simple example

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,25 +1,11 @@
-/*Promises
-- What is a promise?
-    a promise is an assurance or gurantee that something will happen in the
-    future. a person can promise another a specific outcome on result.
-    a promise is an object that holds the future value of an aync operation.
-- Example: reqquesting some data from a server, the promis, promises us to get
-    that data which we can use in future.
-- States of a promise : PENDING FULFILLED REJECTED
-
-*/
-
-const promise = new Promise ((resolve, reject) => {
-    const allWentWell = true;
-    if(allWentWell) {
-        resolve('All good');
-    }else {
-        reject('All wrong');
-    }
+const promise = new Promise((resolve, reject) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    setTimeout(() => {
+        if(randomNumber < 4 ){
+            resolve('You guessed right');
+        } else{
+            reject('You guessed false');
+        }
+    }); 
 });
-/*
-Promise {<fulfilled>: 'All good'}
-[[Prototype]]: Promise
-[[PromiseState]]: "fulfilled"
-[[PromiseResult]]: "All good" */
 console.log(promise);
