ad52a3a0afcf9a3cb19f1e7900159b28d7c120ff
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-08-04 19:26:50 +0300

    promises call

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,11 +1,19 @@
-const promise = new Promise((resolve, reject) => {
-    const randomNumber = Math.floor(Math.random() * 10);
-    setTimeout(() => {
-        if(randomNumber < 4 ){
-            resolve('You guessed right');
-        } else{
-            reject('You guessed false');
-        }
-    }); 
+const promiseOne = new Promise((resolve, reject) => {
+    resolve('promise one is resolved')});
+const promiseTwo = new Promise((resolve, reject) => {
+    resolve('promise two is resolved');
 });
-console.log(promise);
+const promiseThree = new Promise((resolve, reject) => {
+    reject('promise three is rejected');
+});
+promiseOne.then((value) => {
+    console.log(value);
+    return promiseTwo;
+})
+.then((value) => {
+    console.log(value);
+    return promiseThree;
+})
+.catch((err) => {
+    console.log(err)
+});
