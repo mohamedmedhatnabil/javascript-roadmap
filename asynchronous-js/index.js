9ccd9fc5e90966567db289af826b321160e174f8
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-08-04 22:55:31 +0300

    a delete request

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,14 +1,5 @@
 fetch('https://dummyjson.com/products/1', {
-    method: 'PUT', 
-    headers: {
-        'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-       title: 'Iphone 19',
-       description: 'Changed to Iphone 19',
-       price: '1000',
-       rating: '9/10'
-    })
+    method: 'DELETE',
 })
 .then(response => response.json())
 .then(data => console.log(data))
