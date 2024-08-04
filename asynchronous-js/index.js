b22cca1cc762de795bae819856fc94851bb32439
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-08-04 22:50:37 +0300

    make a post request

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,4 +1,14 @@
-fetch('https://dummyjson.com/products/1', {})
+fetch('https://dummyjson.com/products/add', {
+    method: 'POST', 
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+       description: 'Iphone 19',
+       price: '1000',
+       rating: '9/10'
+    })
+})
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(error => console.log(error)) 
